d1e3101482d9a42dd613f3c620088fcf4d796d22
ryohei suzuki <ryousanngataryousann@gmail.com>
2018-09-12 15:26:42 +0900

    　first commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func multi(){
+        number = number * 2
+        label.text = String(number)
+    }
+    
+    @IBAction func division(){
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
+    
 }
 
